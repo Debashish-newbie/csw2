dc6f2049d16d813fff312359bd3a7b76cd669192
Debashish-newbie <palaisrikanta13@gmail.com>
2026-02-18 12:53:06 +0530

    CSW 18/02/26

--- chapter2/mysite/blog/models.py
+++ chapter2/mysite/blog/models.py
@@ -1,10 +1,28 @@
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-class post(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+        
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
     body = models.TextField()
-
+    status = models.CharField(
+        max_length=2, 
+        choices=Status.choices, 
+        default=Status.DRAFT)
+    
+    publish=models.DateTimeField(default=timezone.now)
+    created=models.DateTimeField(auto_now_add=True)#store the date and time when the post is first created
+    updated=models.DateTimeField(auto_now=True) #store the date and time when the post is last updated
+    
+    class Meta:
+        ordering = ('-publish',) #order the posts by publish date in descending order
+        # - means descending order, + means ascending order
+        indexes = [models.Index(fields=['-publish'])] #create an index on the publish field in descending order    
     def __str__(self):
         return self.title
