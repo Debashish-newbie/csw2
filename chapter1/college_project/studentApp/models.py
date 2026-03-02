f9ad4e7b9ef3c5217a2cd852556ba1e7b8279575
Debashish-newbie <palaisrikanta13@gmail.com>
2026-03-02 10:58:09 +0530

    CSW 02/03/26

--- chapter1/college_project/studentApp/models.py
+++ chapter1/college_project/studentApp/models.py
@@ -1,9 +1,26 @@
+
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
 
 class student(models.Model):
     name= models.CharField(max_length=100)
     age = models.IntegerField()
+    roll_no = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    dept = models.CharField(
+        max_length=100,
+        default='CSE',
+        )
+    
     
     def __str__(self):
         return self.name
-        
+    
+    
+    class Meta:
+        ordering = ('-age',) 
+        indexes = [models.Index(fields=['-age'])]  
+    def __str__(self):
+        return self.name   
