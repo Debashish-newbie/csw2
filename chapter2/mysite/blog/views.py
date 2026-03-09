a538e84b0bbac876866e420c07cd04c8544fb25e
Debashish-newbie <palaisrikanta13@gmail.com>
2026-03-09 10:28:22 +0530

    09/03/26

--- chapter2/mysite/blog/views.py
+++ chapter2/mysite/blog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.http import Http404
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+    
 def post_list(request):
-    posts = Post.objects.all()
+    all_posts = Post.objects.all()                                           
+    paginator = Paginator(all_posts, 3)  # Show 3 posts per page
+    page_number = request.GET.get('page',1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     return render(
         request, 'blog/post/list.html', {'posts': posts}
         )
@@ -15,7 +24,7 @@ def post_detail(request, id):
     return render(
         request, 'blog/post/detail.html', {'post': post}
         )
-def post_slug_date_detail(request, year, month, day, slug, Status):
+def post_slug_date_detail(request, year, month, day, slug, status):
     post1 = get_object_or_404(Post, 
                               status=Post.Status.PUBLISHED,
                               slug=slug, 
