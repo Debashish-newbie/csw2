6315389348e4bfbe09a55daea9f066cfd8e5fe87
Debashish-newbie <palaisrikanta13@gmail.com>
2026-03-25 11:55:38 +0530

    CSW MIDUP 25/03/26

--- chapter2/mysite/blog/models.py
+++ chapter2/mysite/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.conf import settings
+
 from taggit.managers import TaggableManager
 
 # Create your models here.
