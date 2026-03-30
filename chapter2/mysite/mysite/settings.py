4d845abfe4bffa7b735910feecc40971c045d622
Debashish-newbie <palaisrikanta13@gmail.com>
2026-03-30 11:00:16 +0530

    CSW 30/02/26

--- chapter2/mysite/mysite/settings.py
+++ chapter2/mysite/mysite/settings.py
@@ -79,10 +79,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB', default='postgres'),
+        'USER': config('POSTGRES_USER', default='blog'),
+        'PASSWORD': config('POSTGRES_PASSWORD', default='79780'),
+        'HOST': config('POSTGRES_HOST', default='localhost'),
+        'PORT': config('POSTGRES_PORT', default='5432'),
     }
 }
 
