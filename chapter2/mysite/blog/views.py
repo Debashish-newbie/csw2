1a9a4c5837df4081218b941aef3b89e0de752e96
Debashish-newbie <palaisrikanta13@gmail.com>
2026-04-01 12:53:56 +0530

    CSW 01/0/26

--- chapter2/mysite/blog/views.py
+++ chapter2/mysite/blog/views.py
@@ -7,8 +7,10 @@ from .forms import EmailPostForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from .forms import CommentForm
+from .forms import SearchForm
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -164,3 +166,21 @@ def post_comment(request, post_id):
          'form':form, 
          'comment': comment}
     )
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query))
+
+    return render(request, 'blog/post/search.html', {
+        'form': form,
+        'query': query,
+        'results': results
+    })
